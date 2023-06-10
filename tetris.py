b9640d661016f4a0e132509e632652dce345adf2
YC CHEN <shflte1314527@gmail.com>
2023-06-10 20:12:28 +0800

    init

--- tetris.py
+++ tetris.py
@@ -26,6 +26,101 @@ import block
 import constants
 import threading
 import socket
+import iot_control.DAN as DAN
+
+import time, requests, random 
+import sys
+import collections.deque as deque
+
+class IoTtalk():
+    # rewrite DAN2.py here
+    def __init__(self):
+        self.serverURL = 'https://4.iottalk.tw'
+        self.mac_addr = 'CD8600D38' + str( random.randint(100,999 ) )
+        self.Reg_addr = self.mac_addr
+        DAN.profile['dm_name']='Dummy_Device'   # you can change this but should also add the DM in server
+        DAN.profile['df_list']=['Dummy_Sensor', 'Dummy_Control']   # Check IoTtalk to see what IDF/ODF the DM has
+        DAN.profile['d_name']= str( random.randint(100,999 ) ) +"_"+ DAN.profile['dm_name'] # None
+        DAN.device_registration_with_retry(self.serverURL, self.Reg_addr) 
+        self.gotInput=False
+        self.theInput="haha"
+        self.allDead=False
+        self.control_queue = deque()
+
+        #creat a thread to do Input data from keyboard, by tsaiwn@cs.nctu.edu.tw 
+        threadx = threading.Thread(target=self.doRead)
+        threadx.daemon = True  # 這樣才不會阻礙到主程式的結束
+        threadx.start()
+
+        while True:
+            try:
+                if(allDead): break;
+            #Pull data from a device feature called "Dummy_Control"
+                value1=DAN.pull('Dummy_Control')
+                if value1 != None:    # 不等於 None 表示有抓到資料
+                    self.send_message(value1[0])
+                    
+            #Push data to a device feature called "Dummy_Sensor" 
+                #value2=random.uniform(1, 10)    ## original Dummy_Device example
+                if gotInput:  # 小弟有讀到資料了 
+                # we have data in theInput
+                    try:
+                        value2=float( theInput )   # 故意轉成實數 
+                    except:
+                        value2=0   # 轉成實數失敗就當作 0.0 
+                    if(allDead): break;
+                    gotInput=False   # so that you can input again  # 讓小弟知道我拿走了  
+                    DAN.push ('Dummy_Sensor', value2,  value2)  #  試這:  DAN.push('Dummy_Sensor', theInput) 
+
+            except Exception as e:
+                print(e)
+                if str(e).find('mac_addr not found:') != -1:
+                    print('Reg_addr is not found. Try to re-register...')
+                    DAN.device_registration_with_retry(self.serverURL, self.Reg_addr)
+                else:
+                    print('Connection failed due to unknow reasons.')
+                    time.sleep(1)    
+            try:
+                time.sleep(0.2)
+            except KeyboardInterrupt:
+                break
+    
+    def send_message(self, val):
+        # send message to the tetris game
+        print("val sent: ", val)
+        if val == 1:
+            self.control_queue.append(b'l')
+        elif val == 2:
+            self.control_queue.append(b'd')
+        elif val == 3:
+            self.control_queue.append(b'r')
+        elif val == 4:
+            self.control_queue.append(b' ')
+            
+
+    def doRead(self):
+        global gotInput, theInput, allDead
+        while True:   
+            while gotInput:   # 老闆還沒把資料拿走
+                time.sleep(0.1)    # 小睡 下把 CPU 暫時讓給別人
+                continue  # go back to while   
+            try:     # 準備讀取資料, 注意程式會卡在這等 User 輸入, 所以要用 Thread
+                theInput = input("Give me data: ")
+            except Exception:    ##  KeyboardInterrupt:
+                allDead = True
+                print("\n\nDeregister " + DAN.profile['d_name'] + " !!!\n",  flush=True)
+                DAN.deregister()
+                sys.stdout = sys.__stdout__
+                print(" Thread say Bye bye ---------------", flush=True)
+                sys.exit(0);   ## break  # raise   #  ?
+            if theInput =='quit' or theInput == "exit":    # these are NOT data
+                allDead = True
+            else:
+                print("Will send " + theInput, end="   , ")
+                gotInput=True   # notify my master that we have data 
+            if allDead: break;   # 離開 while True 這 Loop  
+
+    
 
 class Tetris(object):
     """
@@ -374,4 +469,3 @@ class Tetris(object):
 if __name__ == "__main__":
     Tetris(16,30).run()
 
-#Special add to try pull requests
